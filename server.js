3e587443675932379b45d5ca16225eb04013661d
Silverio Zuniga <silvermoon12286@gmail.com>
2026-02-18 16:55:55 -0600

    PORT

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-const port = process.env.PORT
+const port = process.env.PORT;
+const databaseUrl = process.env.DATABASE_URL;
 const db = require('knex')({
   client: 'pg',
   connection: {
