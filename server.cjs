24dbc2f2d999f9693e08de396ea1e8db669172e3
Silverio Zuniga <silvermoon12286@gmail.com>
2026-02-18 17:36:32 -0600

    Extension

---
+++ server.cjs
@@ -0,0 +1,42 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+import handleRegister from './controllers/register.js';
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const port = process.env.PORT;
+const databaseUrl = process.env.DATABASE_URL;
+const db = require('knex')({
+  client: 'pg',
+  connection: {
+  connectionString: process.env.DATABASE_URL,
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB,
+      }
+  });
+
+db.select('*').from('users').then(data => {
+	console.log(data);
+});
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req, res) => {res.send(database.users)})
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+app.listen(PORT, ()=> {
+	console.log('app is running on PORT ${PORT}')
+})
+
+
