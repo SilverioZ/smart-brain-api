e1dc003bbab8f8717de69001b0a3496fa21bbc6b
SilverioZ <silvermoon12286@gmail.com>
2026-02-20 16:24:40 -0600

    Remove default port fallback in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 const database = process.env.DATABASE_URL;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const db = require('knex')({
   client: 'pg',
   connection: {
